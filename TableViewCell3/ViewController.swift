4fd30a5577edaf643c2685330a23febf440c89e1
ouyou58 <ouyou58@gmail.com>
2019-06-17 23:16:11 +0900

    Initial Commit

---
+++ TableViewCell3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewCell3
+//
+//  Created by ouyou on 2019/06/17.
+//  Copyright © 2019 ouyou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
